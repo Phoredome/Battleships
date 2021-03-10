d12d8a9e1b7d5ba6950794f0aafa0e3a687f1b38
Phoredome <bosco.li.97@hotmail.com>
2021-03-10 11:36:03 -0800

    Move files to new foldere structure

--- src/main/java/com/battleships/BattleshipApplication.java
+++ src/main/java/com/battleships/BattleshipApplication.java
@@ -1,7 +1,19 @@
 package com.battleships;
 
+import com.battleships.gui.BoardGUI;
+
 public class BattleshipApplication {
     public static void main(String[] args) {
-        
+        BoardGUI board = new BoardGUI();
+
+        //setSampleEnemyBoatBoard();
+
+        board.createMainBoard();
+
+        //to call/show the board window
+        //board.setSize(400,400);
+        //board.setVisible(true);
+        //or call the method which makes JFrame from BoardGUI class, like below:
+        //demo();
     }
 }
--- src/main/java/com/example/battleShipv2/model/TileContent.java
+++
@@ -1,5 +0,0 @@
-package com.example.battleShipv2.model;
-
-public enum TileContent {
-    Empty, Alive_Ship, Hit_Ship
-}
